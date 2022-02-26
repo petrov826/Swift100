558400c3ab5f7726ca8aa1ba6b89ce9f217f533f
petrov826 <g8myc3hjz@me.com>
2022-02-26 15:20:59 +0900

    done day17

--- WeSplit/Shared/ContentView.swift
+++ WeSplit/Shared/ContentView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [0, 5, 10, 15, 20, 25]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount * (tipSelection / 100)
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    
     var body: some View {
-        Text("Hello world")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField(
+                        "amount",
+                        value: $checkAmount,
+                        format: .currency(code: Locale.current.currencyCode ?? "USD")
+                    )
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    // note: $numberOfPeople is confusing
+                    // when we choose fisrt element(2 people),
+                    // $numberOfPeople will be 0 not 2.
+                    // because it's 0th element
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                    // Text("\(numberOfPeople)")
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: { // using second trailing closure
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                // show done button above the keyboard
+                // push it right hand side with Spacer
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
