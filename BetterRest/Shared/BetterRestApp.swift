dea7711cb1281386359f949ce1eb6fbaad6e83f1
petrov826 <g8myc3hjz@me.com>
2022-03-16 21:35:21 +0900

    done day26

---
+++ BetterRest/Shared/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/03/14.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
