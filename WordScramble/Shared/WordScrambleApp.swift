4d2af22d94ebb0e0eeee2979b4c47cf1355f336c
petrov826 <g8myc3hjz@me.com>
2022-03-20 14:39:25 +0900

    doing day29
    Introducing List

---
+++ WordScramble/Shared/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/03/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
