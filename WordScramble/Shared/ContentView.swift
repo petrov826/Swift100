07af3218f93a4c7612d502bd65f7c1cc0c814670
petrov826 <g8myc3hjz@me.com>
2022-03-21 14:58:34 +0900

    clean up the code for day30

--- WordScramble/Shared/ContentView.swift
+++ WordScramble/Shared/ContentView.swift
@@ -12,24 +12,6 @@ struct ContentView: View {
         Text("Hello")
             .padding()
     }
-    
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                print(fileContents)
-            }
-        }
-            
-    }
-    
-    func spellCheckTest() {
-        let word = "swift"
-        let checker = UITextChecker()
-        let range = NSRange(location: 0, length: word.utf16.count)
-        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
-        let allGood = misspelledRange.location == NSNotFound
-        print(allGood)
-    }
 }
 
 struct ContentView_Previews: PreviewProvider {
