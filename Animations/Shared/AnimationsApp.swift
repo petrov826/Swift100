ca3b952af11110e0f8a622aa921b531c6e43ce0f
petrov826 <g8myc3hjz@me.com>
2022-03-24 21:50:11 +0900

    done day32

---
+++ Animations/Shared/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
