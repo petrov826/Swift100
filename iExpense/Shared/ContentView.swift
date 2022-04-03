bdc4de2f253cb6732a362c38a19eb0883c732727
petrov826 <g8myc3hjz@me.com>
2022-04-03 22:12:16 +0900

    doing day36: introducing onDelete and EditButton

--- iExpense/Shared/ContentView.swift
+++ iExpense/Shared/ContentView.swift
@@ -7,29 +7,34 @@
 
 import SwiftUI
 
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let name: String
+struct ContentView: View {
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack {
-            Text("Hello \(name)! I'm second view!!")
-            
-            Button("dismiss") {
-                dismiss()
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+            }
+            .navigationTitle("onDelete()")
+            .toolbar {
+                EditButton()
             }
         }
     }
-}
-struct ContentView: View {
-    @State private var showingSheet = false
-    var body: some View {
-        Button("Show Sheet") {
-            showingSheet.toggle()
-        }
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "Peter")
-        }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
     }
 }
 
