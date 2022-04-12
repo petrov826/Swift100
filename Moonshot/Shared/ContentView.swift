88a0f772be285ef84fc071965dd9fb98db6c04c4
petrov826 <g8myc3hjz@me.com>
2022-04-12 22:29:14 +0900

    doing day39: introducing resizable, aspectRatio and GeometryReader

---
+++ Moonshot/Shared/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        GeometryReader { geo in
+            Image("Example")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                // frame height will be inferred
+                // by contentMode: .fit and frame width
+                .frame(
+                    width: geo.size.width * 0.8
+                )
+                // move the image to the center of the screen
+                .frame(
+                    width: geo.size.width,
+                    height: geo.size.height
+                )
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
