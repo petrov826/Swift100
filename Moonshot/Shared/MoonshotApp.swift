1b8188e94e78011ae36e4d8c06b0068eb4d6f88c
petrov826 <g8myc3hjz@me.com>
2022-04-12 22:37:24 +0900

    doing day39: introducing resizable, aspectRatio and GeometryReader

---
+++ Moonshot/Shared/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/04/12.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
