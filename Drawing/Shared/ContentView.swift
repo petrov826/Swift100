68f5819e4525cb74fc838f4ed8f23867c418a12c
petrov826 <g8myc3hjz@me.com>
2022-04-22 20:04:11 +0900

    doing day43: introducing Shape and making Triangle and Arc shape

--- Drawing/Shared/ContentView.swift
+++ Drawing/Shared/ContentView.swift
@@ -7,23 +7,49 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        // x is right arrow, y is down arrow
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+
+struct Arc: Shape {
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAjustment
+        let modifiedEnd = endAngle - rotationAjustment
+        
+        var path = Path()
+        
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.size.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd  , clockwise: !clockwise)
+        
+        return path
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        // drawing a triangle
-        Path { path in
-            // note: CG stands for Core Graphics
-            path.move(to: CGPoint(x: 200, y: 100))
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-            path.closeSubpath()
+        VStack {
+            Triangle()
+                .fill(.green)
+                .frame(width: 300, height: 300)
+            
+            Arc(startAngle: .degrees(0), endAngle: .degrees(270), clockwise: true)
+                .stroke(.red, lineWidth: 10)
+                .frame(width: 300, height: 300)
         }
-        // .fill(.blue) // blue triangle
-        // .stroke(.blue, lineWidth: 10) // blue stroke
-        .stroke(
-            .blue,
-            style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round)
-        )
     }
 }
 
