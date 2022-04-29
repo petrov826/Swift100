52f44be3c46fd6effab4dcbd34743a03581c785c
petrov826 <g8myc3hjz@me.com>
2022-04-29 15:13:25 +0900

    doing day45: AnimatablePair

--- Drawing/Shared/ContentView.swift
+++ Drawing/Shared/ContentView.swift
@@ -7,37 +7,55 @@
 
 import SwiftUI
 
-struct Trapezoid: Shape {
-    var insetAmount: Double
+struct Checkerboard: Shape {
+    // rows and columns CAN be changed over time
+    // so use var instead of let
+    var rows: Int
+    var columns: Int
     
-    var animatableData: Double {
-        get { insetAmount }
-        set { insetAmount = newValue }
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
     }
     
     func path(in rect: CGRect) -> Path {
         var path = Path()
         
-        // clockwise
-        path.move(to: CGPoint(x: 0, y: rect.maxY)) // bottom left
-        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY)) // top left
-        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY)) // top right
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY)) // bottom right
-        path.addLine(to: CGPoint(x: 0, y: rect.maxY)) // bottom left
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
         
         return path
     }
 }
 
 struct ContentView: View {
-    @State private var amount = 5.0
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
-        Trapezoid(insetAmount: amount)
-            .frame(width: 200, height: 200)
+        Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
-                withAnimation {
-                    amount = Double.random(in: 10...90)
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
                 }
             }
     }
