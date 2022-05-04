7fb8c9439312234ca59053b0856916d91abd0d68
petrov826 <g8myc3hjz@me.com>
2022-05-04 13:31:27 +0900

    doing day49: async, await, URLSession and more

--- CupcakeCorner/Shared/ContentView.swift
+++ CupcakeCorner/Shared/ContentView.swift
@@ -7,30 +7,51 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Paul Hudson"
-    
-    // decode data on init
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    // encode data
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                
+                Text(item.collectionName)
+            }
+        }
+        .task {
+            await loadData()
+        }
+    }
+    
+    func loadData() async {
+        // convert string to URL. If failed, stop there
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            // toss away metadata with _
+            // data isn't decoded yet
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedData = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedData.results
+            }
+        } catch {
+            print("Invalid Data")
+        }
     }
 }
 
