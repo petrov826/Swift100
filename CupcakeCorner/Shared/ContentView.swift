981a8d590e9bdce64aef24b2e4088af0edfecd72
petrov826 <g8myc3hjz@me.com>
2022-05-04 13:01:13 +0900

    doing day49: Codable and @Published

---
+++ CupcakeCorner/Shared/ContentView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/05/04.
+//
+
+import SwiftUI
+
+class User: ObservableObject, Codable {
+    enum CodingKeys: CodingKey {
+        case name
+    }
+    
+    @Published var name = "Paul Hudson"
+    
+    // decode data on init
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    // encode data
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupcakeCorner/Shared/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/05/04.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
