c215d9de51400ef8ac8482660008b55e0ac67af1
petrov826 <g8myc3hjz@me.com>
2022-05-20 20:28:49 +0900

    update .gitignore and add a not committed file

---
+++ ViewsAndModifiers/Shared/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/03/06.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
