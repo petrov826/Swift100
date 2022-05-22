0724637b19bb9dfe899f957ec55cd3b465434b02
petrov826 <g8myc3hjz@me.com>
2022-05-22 22:33:37 +0900

    starting day53

---
+++ Bookworm/Shared/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Shared
+//
+//  Created by peterpetrov on 2022/05/22.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
