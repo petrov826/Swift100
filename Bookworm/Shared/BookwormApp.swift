f7d706f46376a74293f35000ed44b33ab7f1ee98
petrov826 <g8myc3hjz@me.com>
2022-05-23 20:18:18 +0900

    done day53: CoreData, DataController, @FetchRequest and more

--- Bookworm/Shared/BookwormApp.swift
+++ Bookworm/Shared/BookwormApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct BookwormApp: App {
+    // source of truth
+    @StateObject private var dataController = DataController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
--- Bookworm/Shared/ContentView.swift
+++ Bookworm/Shared/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("notes") private var notes = ""
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
     
-    var body: some View  {
-        NavigationView {
-            TextEditor(text: $notes)
-                .padding()
-                .navigationTitle("Notes")
+    var body: some View {
+        VStack {
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let firstName = ["Harry", "Ron", "Hermione", "Ginny"]
+                let lastName = ["Potter", "Weasley", "Granger"]
+                
+                let chosenFirstName = firstName.randomElement()!
+                let chosenLastName = lastName.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
         }
     }
 }
