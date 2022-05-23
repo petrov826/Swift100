72d21f7f17778263cc1731fb83a1ea737699a51d
petrov826 <g8myc3hjz@me.com>
2022-05-23 20:22:33 +0900

    clean up the codes for day54

--- Bookworm/Shared/ContentView.swift
+++ Bookworm/Shared/ContentView.swift
@@ -8,29 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
-    
     var body: some View {
-        VStack {
-            List(students) { student in
-                Text(student.name ?? "Unknown")
-            }
-            
-            Button("Add") {
-                let firstName = ["Harry", "Ron", "Hermione", "Ginny"]
-                let lastName = ["Potter", "Weasley", "Granger"]
-                
-                let chosenFirstName = firstName.randomElement()!
-                let chosenLastName = lastName.randomElement()!
-                
-                let student = Student(context: moc)
-                student.id = UUID()
-                student.name = "\(chosenFirstName) \(chosenLastName)"
-                
-                try? moc.save()
-            }
-        }
+        Text("Hello")
     }
 }
 
