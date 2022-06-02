9f01d4347bdf72486f17bb9b459284e2ec758499
petrov826 <g8myc3hjz@me.com>
2022-06-02 21:29:11 +0900

    doing day58: introducing NSPredicate

--- CoreDataProject/Shared/ContentView.swift
+++ CoreDataProject/Shared/ContentView.swift
@@ -9,30 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(
+        sortDescriptors: [],
+        predicate: NSPredicate(format: "universe == 'Star Wars'")
+    ) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(wizards, id: \.self) { wizard in
-                Text(wizard.name ?? "Unknown")
+            List(ships, id: \.self) { ship in
+                Text(ship.name ?? "Unknown name")
             }
             
-            Button("Add") {
-                // without wizard.append(wizard) or something,
-                // new wizard will be append to the wizards
-                // by saying Wizard(context: moc)
-                let wizard = Wizard(context: moc)
-                // without this line, the empty item will be added
-                // ("Unknown" will be displayed)
-                wizard.name = "Harry Potter"
-            }
-            
-            Button("Save") {
-                do {
-                    try moc.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Add Examples") {
+                let ship1 = Ship(context: moc)
+                ship1.name = "Enterprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: moc)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: moc)
+                ship3.name = "Millennium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: moc)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? moc.save()
             }
         }
     }
