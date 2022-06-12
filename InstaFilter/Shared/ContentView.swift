32bd04d72284b91c826b550800a3dcdc01c2a5c1
petrov826 <g8myc3hjz@me.com>
2022-06-12 18:06:13 +0900

    done day62 and clean up the code

--- InstaFilter/Shared/ContentView.swift
+++ InstaFilter/Shared/ContentView.swift
@@ -8,29 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingConfirmation = false
-    @State private var backgroundColor = Color.white
     
     var body: some View {
         Text("Hello everyone")
-            .frame(width: 300, height: 300)
-            .background(backgroundColor)
-            .onTapGesture {
-                showingConfirmation = true
-            }
-            // confirmationDialog has exactly same interface as
-            // alert modifier. we can switch easily
-            .confirmationDialog("Change Background Color", isPresented: $showingConfirmation) {
-                Button("Red") { backgroundColor = .red }
-                Button("Green") { backgroundColor = .green }
-                Button("Blue") { backgroundColor = .blue }
-                // Cancel button is built-in.s
-                // we can override it's text
-                // like "Wait! Please Stop!!"
-                Button("Cancel", role: .cancel) { }
-            } message: {
-                Text("Select a new color")
-            }
     }
 }
 
