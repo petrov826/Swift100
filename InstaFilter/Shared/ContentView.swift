a307474abb3a708a47bc68ac789a9ffd27d470e1
petrov826 <g8myc3hjz@me.com>
2022-06-20 22:11:30 +0900

    doing day65: clean up the code to start day65

--- InstaFilter/Shared/ContentView.swift
+++ InstaFilter/Shared/ContentView.swift
@@ -8,38 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingPicker = false
-    
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                showingPicker = true
-            }
-            
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
-                
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
-            }
-        }
-        .sheet(isPresented: $showingPicker) {
-            ImagePicker(image: $inputImage)
-        }
-        .onChange(of: inputImage) { _ in loadImage() }
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else { return }
-        image = Image(uiImage: inputImage)
-        
-        UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
+        Text("Hello")
     }
 }
 
