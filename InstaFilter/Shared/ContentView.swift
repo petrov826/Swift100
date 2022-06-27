234c4a989a38d5d4e3acc2e638dbcc437857a553
petrov826 <g8myc3hjz@me.com>
2022-06-27 22:24:34 +0900

    done day65
    too complex for me.

--- InstaFilter/Shared/ContentView.swift
+++ InstaFilter/Shared/ContentView.swift
@@ -5,11 +5,89 @@
 //  Created by peterpetrov on 2022/06/06.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    
+    @State private var currentFileter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
     var body: some View {
-        Text("Hello")
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+                    
+                    Text("Tap to select a photo")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture {
+                    showingImagePicker = true
+                }
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity) { _ in applyProcessing() }
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Image") {
+                        //
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Save", action: save)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
+            .onChange(of: inputImage) { _ in loadImage() }
+            .sheet(isPresented: $showingImagePicker) {
+                // when user selected an image, it will be set to inputImage
+                // Then, loadImage() will be called.
+                // Finally, image will be displayed
+                ImagePicker(image: $inputImage)
+            }
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        
+        let beginImage = CIImage(image: inputImage)
+        currentFileter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func save() {
+        
+    }
+    
+    func applyProcessing() {
+        currentFileter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFileter.outputImage else { return }
+        // CIImage -> (context) -> CGImage -> UIImage -> Image(SwiftUI)
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+        }
     }
 }
 
