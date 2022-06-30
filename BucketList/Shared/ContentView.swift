0f702941562ac908008c6812ee9c1998480a397d
petrov826 <g8myc3hjz@me.com>
2022-06-30 22:03:51 +0900

    done day67

--- BucketList/Shared/ContentView.swift
+++ BucketList/Shared/ContentView.swift
@@ -7,30 +7,52 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Text("Hello")
-            .onTapGesture {
-                let message = "Test Message"
-                let url = getDocumentDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    // try to write to the URL
-                    try message.write(to: url, atomically: true, encoding: .utf8)
-                    
-                    // try to read it back
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
+        Text("Loading...")
     }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed.")
+    }
+}
+
+struct ContentView: View {
+    @State var loadingState = LoadingState.loading
     
-    /// get available Document URLs and return the 1st one
-    func getDocumentDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    var body: some View {
+        Group {
+            switch loadingState {
+            case .loading:
+                LoadingView()
+            case .success:
+                SuccessView()
+            case .failed:
+                FailedView()
+            }
+        }
+        .onTapGesture {
+            switch loadingState {
+            case .loading:
+                loadingState = .success
+            case .success:
+                loadingState = .failed
+            case .failed:
+                loadingState = .loading
+            }
+        }
     }
 }
 
