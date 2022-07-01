f3a7a902f6521d07ebb378c9ed00105254d497ae
petrov826 <g8myc3hjz@me.com>
2022-07-01 22:51:12 +0900

    doing day69: introducing MapKit

--- BucketList/Shared/ContentView.swift
+++ BucketList/Shared/ContentView.swift
@@ -4,54 +4,50 @@
 //
 //  Created by peterpetrov on 2022/06/30.
 //
-
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success!")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
 
 struct ContentView: View {
-    @State var loadingState = LoadingState.loading
+    @State private var mapRegion = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    let locations = [
+        Location(
+            name: "Buckinghum Palace",
+            coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)
+        ),
+        Location(
+            name: "Tower of London",
+            coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)
+        )
+    ]
     
     var body: some View {
-        Group {
-            switch loadingState {
-            case .loading:
-                LoadingView()
-            case .success:
-                SuccessView()
-            case .failed:
-                FailedView()
+        NavigationView {
+            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                // MapMarker can't provide additional information
+                // MapMarker(coordinate: location.coordinate)
+                
+                MapAnnotation(coordinate: location.coordinate) {
+                    // we can put any View as map annotation
+                    NavigationLink {
+                        Text(location.name)
+                    } label: {
+                        Circle()
+                            .stroke(.red, lineWidth: 3)
+                            .frame(width: 44, height: 44)
+                    }
+                        
+                }
             }
-        }
-        .onTapGesture {
-            switch loadingState {
-            case .loading:
-                loadingState = .success
-            case .success:
-                loadingState = .failed
-            case .failed:
-                loadingState = .loading
-            }
+            .navigationTitle("London Explorer")
         }
     }
 }
