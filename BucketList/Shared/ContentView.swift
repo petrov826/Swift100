4a32fa8f8a36da33ed6f30de2ca64477a3317049
petrov826 <g8myc3hjz@me.com>
2022-07-01 23:19:29 +0900

    done day69: introducing LocalAuthentication

--- BucketList/Shared/ContentView.swift
+++ BucketList/Shared/ContentView.swift
@@ -4,50 +4,39 @@
 //
 //  Created by peterpetrov on 2022/06/30.
 //
-import MapKit
+import LocalAuthentication
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
-        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
-    )
-    let locations = [
-        Location(
-            name: "Buckinghum Palace",
-            coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)
-        ),
-        Location(
-            name: "Tower of London",
-            coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076)
-        )
-    ]
+    @State private var isLocked = true
     
     var body: some View {
-        NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                // MapMarker can't provide additional information
-                // MapMarker(coordinate: location.coordinate)
-                
-                MapAnnotation(coordinate: location.coordinate) {
-                    // we can put any View as map annotation
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
-                            .frame(width: 44, height: 44)
-                    }
-                        
+        VStack {
+            if isLocked {
+                Text("Locked")
+            } else {
+                Text("Unlocked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthentication, localizedReason: reason) { success, authenticationError in
+                if success {
+                    isLocked = false
+                } else {
+                    
                 }
             }
-            .navigationTitle("London Explorer")
+        } else {
+            // no biometrics
         }
     }
 }
