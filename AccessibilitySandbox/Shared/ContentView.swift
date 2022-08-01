116cd213dc9cccf0d5d1952de7dc6ea5f31e2cf0
petrov826 <g8myc3hjz@me.com>
2022-08-01 20:30:57 +0900

    doing day74: introducing accessibilityHidden and accessibilityElement

--- AccessibilitySandbox/Shared/ContentView.swift
+++ AccessibilitySandbox/Shared/ContentView.swift
@@ -8,37 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPictureIndex = 0
-    
     var body: some View {
-        Image(pictures[selectedPictureIndex])
-            .resizable()
-            .scaledToFit()
-            .onTapGesture(perform: changeIndex)
-            // read label not image name
-            .accessibilityLabel(labels[selectedPictureIndex])
-            // this image is a button not an image
-            // "Tulpips button" is better than "Tulips image"
-            .accessibilityAddTraits(.isButton)
-            .accessibilityRemoveTraits(.isImage)
-    }
-    
-    func changeIndex() {
-        selectedPictureIndex = Int.random(in: 0..<self.pictures.count)
+        VStack {
+            // BYW, who is this girl?
+            Image(decorative: "character")
+                .resizable()
+                .scaledToFit()
+                .accessibilityHidden(true)
+            
+            // these Text are not 1 View.
+            // but they should be combined for better accessibility
+            Text("Your score is")
+            Text("1000")
+                .font(.title)
+        }
+        // option 1
+        // there will be a small pause when voiceover reads them
+        .accessibilityElement(children: .combine)
+        // option 2
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Your score is 1000")
+        
     }
 }
 
