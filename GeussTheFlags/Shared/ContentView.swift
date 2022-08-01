68cdb1a32b1696767fe6e46873995b51d3bda84e
petrov826 <g8myc3hjz@me.com>
2022-08-01 21:10:14 +0900

    doing day75: adding accessibility for Guess the Flag
    I found that App name is wrong. It should be "Guess the Flag" not "Geuss".

--- GeussTheFlags/Shared/ContentView.swift
+++ GeussTheFlags/Shared/ContentView.swift
@@ -28,6 +28,20 @@ struct ContentView: View {
         "UK", "US"
     ].shuffled()
     
+    let labels = [
+        "Estonia": "Flag with three horizontal stripes of equal size. Top stripe blue, middle stripe black, bottom stripe white",
+        "France": "Flag with three vertical stripes of equal size. Left stripe blue, middle stripe white, right stripe red",
+        "Germany": "Flag with three horizontal stripes of equal size. Top stripe black, middle stripe red, bottom stripe gold",
+        "Ireland": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe orange",
+        "Italy": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe red",
+        "Nigeria": "Flag with three vertical stripes of equal size. Left stripe green, middle stripe white, right stripe green",
+        "Poland": "Flag with two horizontal stripes of equal size. Top stripe white, bottom stripe red",
+        "Russia": "Flag with three horizontal stripes of equal size. Top stripe white, middle stripe blue, bottom stripe red",
+        "Spain": "Flag with three horizontal stripes. Top thin stripe red, middle thick stripe gold with a crest on the left, bottom thin stripe red",
+        "UK": "Flag with overlapping red and white crosses, both straight and diagonally, on a blue background",
+        "US": "Flag with red and white stripes of equal size, with white stars on a blue background in the top-left corner"
+    ]
+    
     // this app will show 3 flags
     // (= first 3 elements of countries) at once.
     // pick a correctAnswer from 3 countries
@@ -81,6 +95,13 @@ struct ContentView: View {
                             //     .clipShape(RoundedRectangle(cornerRadius: 15, style: .continuous))
                             //     .shadow(radius: 5)
                             FlagImage(imageName: countries[number])
+                                // without this, voiceover reads flag name
+                                // it won't be a game...
+                                .accessibilityLabel(
+                                    // countries[number] is String and image name
+                                    // we can use it as dict key
+                                    labels[countries[number], default: "Unknown flag"]
+                                )
                                 .rotation3DEffect(
                                     .degrees(tappedFlagNum == number ? rotateDegree : 0),
                                     axis: (x: 0, y: 1, z: 0)
