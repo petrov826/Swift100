60bbddc851511442d9fed593faf71699c6560208
petrov826 <g8myc3hjz@me.com>
2022-08-08 19:52:46 +0900

    doing day79: introducing TabView and .tag

--- HotProspects/Shared/ContentView.swift
+++ HotProspects/Shared/ContentView.swift
@@ -7,40 +7,29 @@
 
 import SwiftUI
 
-@MainActor class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    // user comes from parent or ancestor view
-    // @EnvironmentObject is a property wrapper
-    // for class conforms to ObservableObject
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
 
 struct ContentView: View {
-    // source of truth here
-    @StateObject var user = User()
+    @State private var selectedTab = "One"
     
     var body: some View {
-        VStack {
-            EditView()
-                // share `user` with child views
-                .environmentObject(user)
-            DisplayView()
-                .environmentObject(user)
+        // when `selection` changed,
+        // automatically switch tab which selectedTag is tag
+        TabView(selection: $selectedTab) {
+            Text("tab1") // this will shown in center
+                .onTapGesture {
+                    selectedTab = "Two"
+                }
+                .tabItem {
+                    // this will shown in bottom
+                    Label("one", systemImage: "star")
+                }
+                .tag("One")
+            
+            Text("tab2")
+                .tabItem {
+                    Label("two", systemImage: "circle")
+                }
+                .tag("Two")
         }
     }
 }
