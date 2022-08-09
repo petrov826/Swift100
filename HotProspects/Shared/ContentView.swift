80b02bde32f9c1b1d86f6d30d50fb645150fc127
petrov826 <g8myc3hjz@me.com>
2022-08-10 00:02:09 +0900

    doing day80: removing blur by .interpolation(.none)

--- HotProspects/Shared/ContentView.swift
+++ HotProspects/Shared/ContentView.swift
@@ -8,39 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var output = ""
-    
     var body: some View {
-        Text(output)
-            .task {
-                await fetchReading()
-            }
-    }
-    
-    func fetchReading() async {
-        let fetchTask = Task { () -> String in
-            let url = URL(string: "https://hws.dev/readings.json")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let readings = try JSONDecoder().decode([Double].self, from: data)
-            return "Found \(readings.count) readings"
-        }
-        
-        let result = await fetchTask.result
-        
-        // option1
-//        do {
-//            output = try result.get()
-//        } catch {
-//            print("Download error")
-//        }
-        
-        // option2
-        switch result {
-        case .success(let str):
-            output = str
-        case .failure(let error):
-            output = "Download error: \(error.localizedDescription)"
-        }
+        Image("example")
+            .interpolation(.none) // remove blur
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(.black)
+            .ignoresSafeArea()
     }
 }
 
