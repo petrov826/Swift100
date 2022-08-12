42fd2e633fcfc92a7fa233d3beb2485a220ac587
petrov826 <g8myc3hjz@me.com>
2022-08-13 00:10:31 +0900

    done day81: Adding Swift package dependencies

--- HotProspects/Shared/ContentView.swift
+++ HotProspects/Shared/ContentView.swift
@@ -5,42 +5,20 @@
 //  Created by peterpetrov on 2022/08/08.
 //
 
+import SamplePackage
 import SwiftUI
-import UserNotifications
 
 struct ContentView: View {
+    let possibleNumbers = 1...60
+    
     var body: some View {
-        VStack {
-            Button("Request Permission") {
-                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { succuess, error in
-                    if succuess {
-                        print("All set!")
-                    // else + if let unwrapping
-                    // we can say simply else here
-                    // and unwrap `error` inside else
-                    } else if let error = error {
-                        print("error: \(error.localizedDescription)")
-                    }
-                }
-            }
-            
-            Button("Schedule Notification") {
-                // what to show
-                let content = UNMutableNotificationContent()
-                content.title = "Feed the dogs"
-                content.subtitle = "They look hungry"
-                content.sound = .default
-                
-                // when to show
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-                
-                // create request from content and trigger
-                // UUID().uuidString returns a random string
-                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-                
-                UNUserNotificationCenter.current().add(request)
-            }
-        }
+        Text(results)
+    }
+    
+    var results: String {
+        let selected = possibleNumbers.random(7).sorted()
+        let strings = selected.map(String.init)
+        return strings.joined(separator: ", ")
     }
 }
 
