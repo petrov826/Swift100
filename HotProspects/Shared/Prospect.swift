403c835dba03325e2d3e08774bf0f15f57a7850d
petrov826 <g8myc3hjz@me.com>
2022-08-24 00:31:30 +0900

    doing day84: saving and loading data with UserDefaults and some refactors

--- HotProspects/Shared/Prospect.swift
+++ HotProspects/Shared/Prospect.swift
@@ -19,14 +19,37 @@ class Prospect: Identifiable, Codable {
 @MainActor class Prospects: ObservableObject {
     // @Publish only monitors # of people
     // property changes doesn't re-invoke subscriber's view
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    
+    let saveKey = "SavedData"
     
     init() {
+        // on init, load data from UserDefaults
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                people = decoded
+                return
+            }
+        }
+        
+        // no data found...
         people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
     }
     
     func toggleIsContacted(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
     }
 }
