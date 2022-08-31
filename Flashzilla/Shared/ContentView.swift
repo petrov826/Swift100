8d25eb543713201101ac0aa3802414bb25dca8bd
petrov826 <g8myc3hjz@me.com>
2022-08-31 20:54:50 +0900

    doing day86: zoom effect(doesn't work well on simulator)

--- Flashzilla/Shared/ContentView.swift
+++ Flashzilla/Shared/ContentView.swift
@@ -8,18 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentZoomAmount = 0.0
+    @State private var finalZoomAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            .onTapGesture(count: 2) {
-                print("Double tapped!")
-            }
-            .onLongPressGesture(minimumDuration: 2) {
-                print("Long pressed!")
-            // when the pressing state of the gesture changes
-            } onPressingChanged: { inProgress in
-                print("In progress?: \(inProgress)")
-            }
+        VStack(spacing: 20) {
+            Text("Hello, world!")
+                .scaleEffect(currentZoomAmount + finalZoomAmount)
+            Text("currentZoomAmount: \(currentZoomAmount)")
+            Text("finalZoomAmount: \(finalZoomAmount)")
+        }
+        .gesture(
+            MagnificationGesture()
+                .onChanged { amount in
+                    print(amount)
+                    currentZoomAmount = amount - 1
+                }
+                .onEnded { amount in
+                    finalZoomAmount += currentZoomAmount
+                    currentZoomAmount = 0.0
+                }
+        )
     }
 }
 
