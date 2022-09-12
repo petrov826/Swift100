66f956ad5854d50eecde9a1055caddb567dee397
petrov826 <g8myc3hjz@me.com>
2022-09-12 21:45:21 +0900

    doing day86: testing some haptic feedback

--- Flashzilla/Shared/ContentView.swift
+++ Flashzilla/Shared/ContentView.swift
@@ -8,37 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset = CGSize.zero
-    @State private var isDragging = false
-    
     var body: some View {
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                offset = value.translation
-            }
-            .onEnded { _ in
-                offset = .zero
-                isDragging = false
-            }
-        
-        let longPressGesture = LongPressGesture()
-            .onEnded { _ in
-                withAnimation {
-                    isDragging = true
+        VStack {
+            Text("Test success feedback")
+                .padding()
+                .onTapGesture {
+                    getFeedback(.success)
                 }
-            }
-        
-        // longPressGesture -> dragGesture
-        // this will save us from misoperation(drag)
-        let combinedGesture =
-            longPressGesture.sequenced(before: dragGesture)
-        
-        Circle()
-            .fill(.red)
-            .frame(width: 64, height: 64)
-            .scaleEffect(isDragging ? 1.25 : 1.0)
-            .offset(offset)
-            .gesture(combinedGesture)
+            
+            Text("Test warning feedback")
+                .padding()
+                .onTapGesture {
+                    getFeedback(.warning)
+                }
+            
+            Text("Test error feedback")
+                .padding()
+                .onTapGesture {
+                    getFeedback(.error)
+                }
+        }
+    }
+    
+    func getFeedback(_ feedbackType: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(feedbackType)
     }
 }
 
