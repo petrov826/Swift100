3faa8c17e25d56b592dc69ea7065c6e562210570
petrov826 <g8myc3hjz@me.com>
2022-09-14 21:38:39 +0900

    doing day87: introducing Timer

--- Flashzilla/Shared/ContentView.swift
+++ Flashzilla/Shared/ContentView.swift
@@ -5,21 +5,23 @@
 //  Created by peterpetrov on 2022/08/30.
 //
 
-import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var counter = 0
+    
     var body: some View {
         VStack {
-            Text("Hello")
-            // Spacer doesn't respond tapping
-            Spacer()
-                .frame(height: 100)
-            Text("World")
-        }
-        .contentShape(Rectangle())
-        .onTapGesture {
-            print("VStack tapped")
+            Text("Hello world")
+                .onReceive(timer) { time in
+                    if counter == 3 {
+                        timer.upstream.connect().cancel()
+                    } else {
+                        print("The time is now \(time)")
+                    }
+                    counter += 1
+                }
         }
     }
 }
