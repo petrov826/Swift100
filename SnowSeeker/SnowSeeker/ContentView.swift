15c1a0f68260014c2881459ebc6845c24881267e
petrov826 <g8myc3hjz@me.com>
2022-10-17 20:53:06 +0900

    doing day96: .sheet() with optionals

--- SnowSeeker/SnowSeeker/ContentView.swift
+++ SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,20 +7,23 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
     var body: some View {
-        NavigationView {
-            NavigationLink {
-                Text("New secondary")
-            } label: {
-                Text("Hello world")
+        Text("Hello world")
+            .onTapGesture {
+                selectedUser = User()
             }
-            .navigationTitle("Primary")
-            
-            Text("Secondary")
-            
-            Text("Tertiary")
-        }
+            // if selectedUser is not nil, show the sheet
+            .sheet(item: $selectedUser) { user in
+                // the type of `user` is User not User?
+                // no unwrapping needed
+                Text(user.id)
+            }
     }
 }
 
